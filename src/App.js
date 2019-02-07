f659b935e215a12d2f3564867afbb2bba5f76a92
enochxoo <yl3744@columbia.edu>
2019-02-06 20:43:10 -0500

    main register

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { TopBar } from './TopBar';
+import { Main } from './Main';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <div className="App-title">
-            Around
-          </div>
-        </header>
-      </div>
+        <div className="App">
+          <TopBar/>
+          <Main/>
+        </div>
     );
   }
 }
